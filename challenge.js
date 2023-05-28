8b82b9bd563bf85fd10600a1dce4093ef24f2f69
Your Name <mayasinalu@gmail.com>
2023-05-28 16:34:54 +0300

    Football points challenge

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
-// Write your code here.
+
+
+function calculatePoints(win, draw, loss) {
+    const points = {
+      win: 3,
+      draw: 1,
+      loss: 0,
+    };
+
+    const total = points.win * win + points.draw * draw + points.loss * loss;
+
+    return total;
+  }
+
+  // Example usage:
+  console.log(calculatePoints(2, 2, 5)); // Output: 8
